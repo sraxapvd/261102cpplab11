a6495b340c3a8ca16ccce81178100bc1864dbbb6
Rada <sirada_dee@cmu.ac.th>
2025-12-26 14:47:35 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future."<<"\n";
+
+    for(int i = 0; i < 3; i++) cin.get();
+
+    srand(time(0));
+    int x = rand()%9 ;
+    cout << "You will get " << grade[x] << " in this 261102.";
+
+    return 0;
+}
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
